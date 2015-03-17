c2e914bd057e98b7417fe88ae42e21353cac6d29
mynomoto <mynomoto@gmail.com>
2015-03-17 08:16:22 -0300

    Update ancient-clj dependency and bump version

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject hoplon-castra/lein-template "0.3.0"
+(defproject hoplon-castra/lein-template "0.4.0"
   :description        "Create new Hoplon projects with Castra"
   :url                "http://github.com/tailrecursion/hoplon-castra-template/"
   :license            {:name  "Eclipse Public License"
                        :url   "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[ancient-clj "0.1.10"]]
+  :dependencies [[ancient-clj "0.3.6" :exclusions [com.amazonaws/aws-java-sdk-s3]]
+                 [com.amazonaws/aws-java-sdk-s3 "1.9.0" :exclusions [joda-time]]]
   :eval-in-leiningen true)
